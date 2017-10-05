50ea486a4404d59fd7603565d4e1eb29444f30a9
bedik16 <berkdikici93@gmail.com>
2017-10-05 11:27:27 +0000

    Added BOX class

--- main.cpp
+++ main.cpp
@@ -3,24 +3,37 @@
 #include <unistd.h>
 #include <string>
 #include "GPIO.h"
+#include "BOX.h"
 
 using namespace std;
 
 int main(void)
 {
-	GPIO lamp_1("4");
-	cout << " on \n" << flush;
-	lamp_1.set();
-	sleep(3);
-	lamp_1.reset();
-	cout << " off \n" << flush;
-	sleep(3);
-	cout << " on \n" << flush;
-	lamp_1.set();
-	sleep(3);
-	lamp_1.reset();
-	cout << " off \n" << flush;
-	sleep(3);
+  BOX box;
+  box.set_rasp();
+  cout << " on \n" << flush;
+  sleep(3);
+  box.reset_rasp();
+  cout << " off \n" << flush;
+  sleep(3);
+  cout << " on \n" << flush;
+  box.set_rasp();
+  sleep(3);
+  box.reset_rasp();
+  cout << " off \n" << flush;
+  sleep(3);
 
-	return 0;
+  return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
